6f3bab112ba20d33b87a1f4fba56fe7e29864286
thl024 <lamtimothy1996@gmail.com>
2018-04-17 15:20:00 -0700

    Init

---
+++ ems/models/ambulance.py
@@ -0,0 +1,2 @@
+class Ambulance():
+    pass
---
+++ ems/simulator.py
@@ -0,0 +1,8 @@
+# from models.ambulance import Ambulance
+# import models.Ambulance
+import Algorithms.models.ambulance
+
+class Simulator():
+
+    def __init__(self, settings):
+        pass
