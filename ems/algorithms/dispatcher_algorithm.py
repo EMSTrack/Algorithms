0de787911967c75e2d7018ee7890f397e3cc6d4d
thl024 <lamtimothy1996@gmail.com>
2018-04-29 15:16:11 -0700

    Cleaned up algorithm

--- ems/algorithms/dispatcher_algorithm.py
+++ ems/algorithms/dispatcher_algorithm.py
@@ -1,4 +1,5 @@
 # The following functions define default algorithms for the DispatchAlgorithm class.
+import numpy as np
 from datetime import timedelta
 from typing import List
 
@@ -7,12 +8,11 @@ from ems.data.traveltimes import TravelTimes
 from ems.models.ambulance import Ambulance
 from ems.models.case import Case
 from ems.models.demand import Demand
+from ems.utils import closest_distance
 
 
 # An implementation of a "fastest travel time" algorithm from a base to
 # the demand point closest to a case
-from ems.utils import closest_distance
-
 
 class DispatcherAlgorithm(Algorithm):
 
@@ -29,60 +29,36 @@ class DispatcherAlgorithm(Algorithm):
             case.closest_demand = closest_distance(demands, case.location)
 
         # Select an ambulance to attend to the given case and obtain the its duration of travel
-        chosen_ambulance, ambulance_travel_time = self.find_available_ambulance(
+        chosen_ambulance, ambulance_travel_time = self.find_fastest_ambulance(
             ambulances, self.traveltimes, case.closest_demand)
 
         return chosen_ambulance, ambulance_travel_time
 
-    def find_available_ambulance(self, ambulances, traveltimes, demand):
+    def find_fastest_ambulance(self, ambulances, traveltimes, demand):
         """
-        Find an available ambulance if possible.
-        :param ambulances: of type dictionary.
+        Finds the ambulance
+        :param ambulances:
         :param traveltimes:
         :param demand:
         :return: type int the ID of the ambulance, or None if all ambulances are busy.
         """
 
-        ambulance_bases = [a.base if not a.deployed else None for a in ambulances]
-        # print('Length of ambulance locations:', len(ambulance_locations))
-        # print('FA:', ambulance_locations)
-        result, case_time = self.closest_time(ambulance_bases, traveltimes, demand)
-        # print('Position:',result)
-        if result > -1:
-            return result, case_time
+        shortest_time = timedelta(hours=9999999)
+        position = -1
 
-        # for amb in ambulances:
-        #     if amb['deployed'] == False:
-        #         distance =
+        for index, amb in enumerate(ambulances):
+            if not amb.deployed:
+                time = self.find_traveltime(traveltimes, amb.base, demand)
+                if shortest_time > time:
+                    shortest_time = time
+                    position = index
+
+        if position > -1:
+            return position, shortest_time
 
         return None, None
 
-    def closest_time(self, list_type, traveltimes, demand):
-        """
-        Finds the ambulance, given a list of ambulances, that will reach the
-        demand point the closest. The demand point must be in the list of demands.
-        IF it is not, then use the above function `closest_distance` to find the
-        closest demand point given a GPS coordinate.
-        The parameter names are bad and the code needs to be refactored, probably
-        :param list_type:
-        :param traveltimes:
-        :param demand:
-        :return:
-        """
-        shortest_difference = timedelta(hours=999999999)
-        position = -1
-        for index in range(len(list_type)):
-
-            if list_type[index] is not None:
-
-                difference = self.traveltime(traveltimes, list_type[index], demand)
-                if shortest_difference > difference:
-                    shortest_difference = difference
-                    position = index
-
-        return position, shortest_difference
-
-    def traveltime(self, traveltimes, base, demand):
+    def find_traveltime(self, traveltimes, base, demand):
         """
         Takes the travel time mapping, starting base, and ending demand to find time.
         :param base:
