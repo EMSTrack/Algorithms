8844e414e3fbb05b589ca69549edb97038cce5f9
thl024 <lamtimothy1996@gmail.com>
2018-04-30 12:22:34 -0700

    First amortization implementation

--- ems/data/tijuana.py
+++ ems/data/tijuana.py
@@ -31,20 +31,35 @@ class CSVTijuanaDataset(Dataset):
         self.set_closest_demand_points(cd_mapping_filepath, self.cases, self.demands)
 
     # Helper functions
-    def set_closest_demand_points(self, file, cases, demands):
+    def set_closest_demand_points(self, filename, cases, demands):
 
-        if file is not None and os.path.exists(file):
+        if filename is not None and os.path.exists(filename):
+
+            cd_mapping = self.read_cd_mapping(filename)
+
             for case in cases:
-                pass
+
+                # TODO somehow map to the actual DEMAND instead of ID
+                for demand in demands:
+                    if demand.id == cd_mapping[case.id]:
+                        case.closest_demand = demand
+
         else:
+            cd_mapping = {}
             for case in cases:
+
                 closest_demand = closest_distance(demands, case.location)
                 case.closest_demand = closest_demand
 
-    def read_cases(self, file):
+                cd_mapping[case.id] = closest_demand.id
+
+            if filename is not None:
+                self.write_cd_mapping(filename, cd_mapping)
+
+    def read_cases(self, filename):
         # Read cases from CSV into a pandas dataframe
         case_headers = ["id", "lat", "long", "date", "weekday", "time", "priority"]
-        cases_df = parse_headered_csv(file, case_headers)
+        cases_df = parse_headered_csv(filename, case_headers)
 
         # Aggregates columms 'date' and 'time' to produce a column for datetime objects
         # TODO -- SLOW OPERATION - find a better way to parse date and time to datetime object
@@ -66,11 +81,11 @@ class CSVTijuanaDataset(Dataset):
 
         return cases, cases_df
 
-    def read_bases(self, file):
+    def read_bases(self, filename):
         # Read bases from an unheadered CSV into a pandas dataframe
         base_col_positions = [4, 5]
         base_headers = ["lat", "long"]
-        bases_df = parse_unheadered_csv(file, base_col_positions, base_headers)
+        bases_df = parse_unheadered_csv(filename, base_col_positions, base_headers)
 
         # Generate list of models from dataframe
         bases = []
@@ -83,11 +98,11 @@ class CSVTijuanaDataset(Dataset):
 
         return bases, bases_df
 
-    def read_demands(self, file):
+    def read_demands(self, filename):
         # Read demands from an unheadered CSV into a pandas dataframe
         demand_col_positions = [0, 1]
         demand_headers = ["lat", "long"]
-        demands_df = parse_unheadered_csv(file, demand_col_positions, demand_headers)
+        demands_df = parse_unheadered_csv(filename, demand_col_positions, demand_headers)
 
         # Generate list of models from dataframe
         demands = []
@@ -100,13 +115,29 @@ class CSVTijuanaDataset(Dataset):
 
         return demands, demands_df
 
-    def read_times(self, file):
+    def read_times(self, filename):
         # Read travel times from CSV file into a pandas dataframe
-        traveltimes_df = pd.read_csv(file)
+        traveltimes_df = pd.read_csv(filename)
 
         traveltimes = TravelTimes(traveltimes_df)
 
         return traveltimes
+
+    def write_cd_mapping(self, filename, mapping):
+        # Generate pandas dataframe and save to CSV
+        df = pd.DataFrame(list(mapping.items()), columns=["Case", "Closest Demand"])
+        df.set_index("Case", inplace=True)
+        df.to_csv(filename)
+
+    def read_cd_mapping(self, filename):
+        # Read CSV file to dataframe and convert to dictionary
+        df = pd.read_csv(filename)
+
+        cd_mapping = {}
+        for index, row in df.iterrows():
+            cd_mapping[int(row["Case"])] = int(row["Closest Demand"])
+
+        return cd_mapping
 
     # Implementation
     def get_bases(self):
