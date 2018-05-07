949bcd8b1cf16cdf7c5f50013d37e7ff8131ebf9
Hans Yuan <hanzzeh@gmail.com>
2018-05-07 01:58:45 -0700

    12 secs for 3 ambs

--- ems/settings.py
+++ ems/settings.py
@@ -10,8 +10,8 @@ class Settings:
                  bases_file: str = None,
                  cases_file: str = None,
                  traveltimes_file: str = None,
-                 num_ambulances: int = 11,
-                 num_bases: int = 11):
+                 num_ambulances: int = 3,
+                 num_bases: int = 3):
 
         # TODO: Look for a settings.yaml file
         
