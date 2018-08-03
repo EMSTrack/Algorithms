cc5156a3d3e48219a06e60630af241e8d071edc2
thl024 <lamtimothy1996@gmail.com>
2018-08-02 21:48:26 -0700

    Event fields modified to include origin and destination location points

--- ems/models/event.py
+++ ems/models/event.py
@@ -1,12 +1,13 @@
 from ems.models.event_type import EventType
+from ems.models.location_point import LocationPoint
 
 
 class Event:
 
     def __init__(self,
-                 timestamp,
-                 destination,
+                 origin: LocationPoint,
+                 destination: LocationPoint,
                  event_type: EventType):
-        self.timestamp = timestamp
+        self.origin = origin
         self.destination = destination
         self.event_type = event_type
