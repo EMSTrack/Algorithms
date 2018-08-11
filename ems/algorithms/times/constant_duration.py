e0f20f867cd48e3ec1d36a6dd8c006e9ac604efc
thl024 <lamtimothy1996@gmail.com>
2018-08-10 19:01:25 -0700

    Constant Duration Algorithm

---
+++ ems/algorithms/times/constant_duration.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta
+
+from geopy import Point
+
+from ems.algorithms.times.duration_algorithm import DurationAlgorithm
+from ems.models.ambulance import Ambulance
+from ems.models.case import AbstractCase
+
+
+class ConstantDurationAlgorithm(DurationAlgorithm):
+    """
+    Implementation of a DurationAlgorithm that returns a constant duration
+    """
+
+    def __init__(self, constant: timedelta):
+        self.constant = constant
+
+    def compute_duration(self,
+                         ambulance: Ambulance,
+                         case: AbstractCase,
+                         origin: Point,
+                         destination: Point,
+                         current_time: datetime):
+        return self.constant
