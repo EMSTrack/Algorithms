6b36c5f4201513522c1ea4105f30ad307e387ef5
Hans Yuan <hanzzeh@gmail.com>
2018-11-28 22:02:03 -0800

    vis of lines made

--- visuals/vis.py
+++ visuals/vis.py
@@ -7,43 +7,45 @@ import matplotlib.animation as animation
 from IPython import embed
 
 def update_line(num, data, line):
-    line.set_data(data[..., :num])
+    # line.set_data(data[..., :num])
+    line.set_data(data[..., num])
     return line,
 
 def ani1():
     fig1 = plt.figure()
 
     # Fixing random state for reproducibility
-    np.random.seed(19680801)
+    # np.random.seed(19680801)
 
     data = np.random.rand(2, 25)
-    l, = plt.plot([], [], 'r-')
+    l, = plt.plot([], [], 'bo')
     plt.xlim(0, 1)
     plt.ylim(0, 1)
     plt.xlabel('x')
     plt.title('test')
-    line_ani = animation.FuncAnimation(fig1, update_line, 25, fargs=(data, l),
-                                       interval=50, blit=True)
+    line_ani = animation.FuncAnimation(fig1, update_line, len(data[0]), fargs=(data, l),
+                                       interval=50, blit=False)
 
     # To save the animation, use the command: line_ani.save('lines.mp4')
+    line_ani.save('lols.mp4')
     plt.show()
 
 def ani2():
     fig2 = plt.figure()
 
-    x = np.arange(-19, 20)
-    y = np.arange(-19, 20).reshape(-1, 1)
+    x = np.arange(-9, 10)
+    y = np.arange(-9, 10).reshape(-1, 1)
     base = np.hypot(x, y)
     ims = []
 
-    for add in np.arange(20):
+    for add in np.arange(30):
         ims.append((plt.pcolor(x, y, base + add, norm=plt.Normalize(0, 30)),))
 
-    im_ani = animation.ArtistAnimation(fig2, ims, interval=50, repeat_delay=300,
+    im_ani = animation.ArtistAnimation(fig2, ims, interval=100, repeat_delay=None,
                                        blit=True)
 
     # To save this second animation with some metadata, use the following command:
-    # im_ani.save('im.mp4', metadata={'artist':'Guido'})
+    im_ani.save('im.mp4', metadata={'artist':'Guido'}, fps=30)
 
     plt.show()
 
@@ -69,4 +71,4 @@ def ani3():
                                   interval=1, blit=True)
     plt.show()
 
-ani3()
+ani1()
