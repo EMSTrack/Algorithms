c4cd6b54418bbe12bc3093445026118dc546e57b
Hans Yuan <hanzzeh@gmail.com>
2018-12-01 18:46:55 -0800

    RegionalVisualizer sends pandas rows to AmbulanceVisualizer to convert primitive types into useful data types

--- visuals/animate_multiple.py
+++ visuals/animate_multiple.py
@@ -26,11 +26,36 @@
     # Using the duration and the start time, calculate the new start time. Repeat until
     # no more paths to take.
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import re
+from geopy import Point
 
 
-srcfile = '../results/processed_cases.csv'
+class Dot:
+    """ Represents the location of the ambulance over time.  """
+    def __init__(self, location, starting_time, duration):
+        pass
 
-class PathVisualizer:
+
+class Line:
+    """ Represents the edge of the ambulance over time. """
+    def __duration_as_dots(self):
+        """
+        Longer distance shown by distance of the line.
+        Longer time shown by the number of dots that get drawn before it finishes.
+        """
+
+        return None
+
+    def __init__(self, starting_point, ending_point, starting_time, duration):
+        """ Defines the points between the starting point and ending point. """
+        pass
+
+
+class AmbulanceTrip:
     """ Each case should have three instances of these enumerated.
     Takes the data from the CSV and changes into start dot, end dot, and
     range of dots that is going to be displayed at each second of the simulation.
@@ -40,9 +65,89 @@ class PathVisualizer:
     Let's say one dot per minute?
     """
 
-    pass
+    def __location(self):
+        """Convert lat, lons from doubles into geopy.Points"""
+
+        names = set()
+        for k in self.row:
+            if any([
+                re.match("[A-Z]*[a-z]*_latitude", k),
+                re.match("[A-Z]*[a-z]*_longitude", k)]
+            ):
+                names.add(k.split("_")[0])
+
+        for location_name in names:
+            lat = self.row[location_name + "_latitude"]
+            lon = self.row[location_name + "_longitude"]
+            self.row[location_name + "_point"] = Point(lat, lon)
+            del self.row[location_name + "_latitude"]
+            del self.row[location_name + "_longitude"]
+
+    def __duration(self):
+        """ For each ___duration, convert into timedelta.  """
+        for k in self.row:
+            if "_duration" in k:
+                self.row[k] = pd.to_timedelta(self.row[k])
+
+    def __init__(self, row):
+        """
+        A single trip of an ambulance has any number of lines.
+        Alternates between stationary and traveling.
+        :param amb_id:
+        :param starts:
+        :param ends:
+        :param durations:
+        """
+
+        self.row = {k: row[k].values[0] for k in row}
+
+        # cast the appropriate types
+        self.__location()
+        self.__duration()
+
+    def __str__(self):
+        return str(self.row)
+
 
 class RegionalVisualizer:
-    """ Total number of cases """
+    """ Basically a 3d Array: time x ambulances x dot positions """
+
+    def __init_ambulances(self, count):
+        """ Generates a list of colors for each ambulance, randomly. """
+        self.ambulance_colors = []
+        for i in range(0, count):
+            self.ambulance_colors.append(None)
+
     def __init__(self, source_file):
-        pass
+        """
+        Get the starting location for each path
+        Get the number of ambulances
+        Read in the cases, convert them into paths
+        Put each path into the list.
+
+        :param source_file:
+        """
+
+        raw_data = pd.read_csv(source_file)
+        self.ambulance_trips = []
+
+        for index in raw_data.index:
+            row = raw_data.iloc[[index]]
+            a = AmbulanceTrip(row)
+            self.ambulance_trips.append(a)
+
+    def __str__(self):
+        return str(self.ambulance_trips)
+
+def main():
+    srcfile = '../results/processed_cases.csv'
+    r = RegionalVisualizer(srcfile)
+
+    for a in r.ambulance_trips:
+        print(a)
+
+
+if __name__ == "__main__":
+    print("main starting")
+    main()
+    print("main ended")
