c5c1c5ea5aa042dff08ce44a3b4c05b586198465
Hans Yuan <hanzzeh@gmail.com>
2018-12-03 21:03:09 -0800

    dots and lines implemented

--- main.py
+++ main.py
@@ -91,8 +91,8 @@ base_set = KDTreeLocationSet(chosen_bases)
 # Define random case params
 # This example = 4 cases an hour
 
-num_cases = 200
-timeframe = timedelta(hours=20)
+num_cases = 30
+timeframe = timedelta(hours=2)
 
 initial_time = datetime.now() - timeframe
 end_time = datetime.now()
--- visuals/animate_plots.py
+++ visuals/animate_plots.py
@@ -23,6 +23,7 @@ class Animator:
                     # print(index_start, ambulance_id, xs, ys)
 
         self.frames = frames
+        embed()
         self.duration = duration
         self.ambulance_bases = ambulance_bases # This is actually a bit of a misnomer.
         # ^ This should be the ambulance's base location, not a list of bases.
@@ -37,12 +38,13 @@ class Animator:
         last_position = len(xs)
 
         while start_position < end_position:
-            
+
             frames[curr_index][ambulance_id][0] += xs[start_position: end_position]
             frames[curr_index][ambulance_id][1] += ys[start_position: end_position]
 
             if end_position < last_position:
                 end_position += 1
+
             if end_position - start_position > display or end_position == last_position:
                 start_position += 1
 
@@ -62,7 +64,29 @@ class Animator:
             for amb_index in range(len(self.frames[frame_index])):
                 xs = self.frames[frame_index][amb_index][0]
                 ys = self.frames[frame_index][amb_index][1]
+
+                # If it moved from its previous location, then it's line [0]
+                # Else, it's dot [1]
+                # if frame_index != 0:
+                #     prev_xs = self.frames[frame_index - 1][amb_index][0]
+                #     prev_ys = self.frames[frame_index - 1][amb_index][1]
+                #
+                #     if prev_xs and prev_ys and xs and ys:
+                #         if xs[-1] == prev_xs[-1] and ys[-1] == prev_ys[-1]:
+                #             plots[amb_index][0].set_data(xs, ys)
+                #             continue
+
+                # if len(xs) != len(set(xs)): embed()
+                if len(xs) > 1:
+                    if xs[0] == xs[1]:
+                        plots[amb_index][1].set_data([xs[0]], [ys[0]])
+                    if xs[-2] == xs[-1]:
+                        plots[amb_index][1].set_data([xs[-1]], [ys[-1]])
+
                 plots[amb_index][0].set_data(xs, ys)
+
+
+
 
             return plots,
 
@@ -72,21 +96,26 @@ class Animator:
 
         plots = []
         for _ in range(len(self.ambulance_bases)):
+
             new_color = 'blue'
+
             line_plot, = plt.plot([], [],
                              marker='+',
                              linestyle='',
                              markerfacecolor=new_color,
                              markeredgecolor=new_color
                              )
+
             dot_plot, = plt.plot([], [],
                              marker='o',
                              linestyle='',
                              markerfacecolor=new_color,
                              markeredgecolor=new_color
                              )
+
             plots.append([line_plot, dot_plot])
 
+        # TODO Make boundaries parameters
         plt.xlim(-117.173017, -116.744906)
         plt.ylim(32.367460, 32.619161)
 
