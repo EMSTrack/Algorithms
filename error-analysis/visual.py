9af67b03579d95a89b44c829a108b3f592e4a382
Hans Yuan <hanzzeh@gmail.com>
2019-01-27 15:14:18 -0800

    error vis completed

--- error-analysis/visual.py
+++ error-analysis/visual.py
@@ -2,8 +2,10 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import yaml
+from IPython import embed
 
-test_figs = True
+test_figs = False
 
 def test():
 	t1 = np.arange(0.0, 5.0, 0.1)
@@ -12,27 +14,44 @@ def test():
 	plt.plot(np.cos(2 * np.pi * t2), 'r--')
 	plt.plot(np.sin(2 * np.pi * t2), 'b+')
 
+def read_data(filename):
+	with open(filename) as input_yaml:
+		return yaml.load(input_yaml.read())
+
 def preconfigurations():
-	plt.xlim(0, 75)
-	plt.ylim(-1, 1)
-
-
-def set_polygon(filename):
+	# plt.xlim(0, 75)
+	# plt.ylim(-1, 1)
 	pass
 
-def set_points(filename, color='b'):
+def set_polygon(coordinates):
+	ys = coordinates['latitude']
+	xs = coordinates['longitude']
+
+	xs.append(xs[0])
+	ys.append(ys[0])
+
+	plt.plot(xs, ys, 'black')
+
+def set_points(coordinates, style='b'):
 	""" Use with real points as well as sim points"""
-	pass
+	ys = coordinates['latitude']
+	xs = coordinates['longitude']
+
+	plt.plot(xs, ys, style)
+
 
 
 def main():
 
 	if test_figs: test()
 
-	preconfigurations()					# Rectangle, xlim, name, etc
-	set_polygon("")						# Read the polygon coordinates into the vis
-	set_points("", color="yo")			# Read the simulator points
-	set_points("", color="ro")			# Read the generated points (real)
+	data = read_data('./error-data.yaml')
+
+	preconfigurations()									# Rectangle, xlim, name, etc
+	set_polygon(data['polygon'])						# Read the polygon coordinates into the vis
+
+	set_points(data['real_locs'],  style="bo")
+	set_points(data['sim_locs'], style="ro")
 
 	plt.show()							# Go.
 
