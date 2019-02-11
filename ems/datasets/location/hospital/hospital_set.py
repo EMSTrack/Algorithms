c5ef60bf93384455de2e367b8510cd2908afe6ef
thl024 <lamtimothy1996@gmail.com>
2019-02-10 16:01:31 -0800

    Headered reading

--- ems/datasets/location/hospital/hospital_set.py
+++ ems/datasets/location/hospital/hospital_set.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from ems.datasets.location.kd_tree_location_set import KDTreeLocationSet
-from ems.utils import parse_unheadered_csv
+from ems.utils import parse_headered_csv
 
 
 class HospitalSet(KDTreeLocationSet):
@@ -15,16 +15,15 @@ class HospitalSet(KDTreeLocationSet):
         super().__init__(latitudes, longitudes)
 
     def read_hospitals(self, filename):
-        # Read hospitals from an unheadered CSV into a pandas dataframe
-        hospital_col_positions = [0, 1]
-        hospital_headers = ["lat", "long"]
-        hospitals_df = parse_unheadered_csv(filename, hospital_col_positions, hospital_headers)
+        # Read hospitals from a headered CSV into a pandas dataframe
+        hospital_headers = ["latitude", "longitude"]
+        hospitals_df = parse_headered_csv(filename, hospital_headers)
 
         # Generate list of models from dataframe
         latitudes = []
         longitudes = []
         for index, row in hospitals_df.iterrows():
-            latitudes.append(row["lat"])
-            longitudes.append(row["long"])
+            latitudes.append(row["latitude"])
+            longitudes.append(row["longitude"])
 
         return latitudes, longitudes
