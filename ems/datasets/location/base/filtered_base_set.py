eb6435747da13a533a4d0ef51720647af19498d6
Hans Yuan <hanzzeh@gmail.com>
2019-02-11 23:23:38 -0800

    k-means now measures double coverage, which can be used to determine which base set might be more preferable. for example, if alpha = 0.95, we can choose any base set with primary coverage 0.95 and up

--- ems/datasets/location/base/filtered_base_set.py
+++ ems/datasets/location/base/filtered_base_set.py
@@ -7,18 +7,21 @@ from ems.datasets.travel_times.travel_times import TravelTimes
 from ems.utils import parse_headered_csv
 
 
+
 class FilteredBaseSet(KDTreeLocationSet):
 
     def __init__(self,
                  count: int,
-                 required_travel_time: int,
+                 r1: int,
+                 r2: int,
                  travel_times: TravelTimes,
                  filename: str = None,
                  latitudes: List[float] = None,
                  longitudes: List[float] = None):
         self.filename = filename
         self.count = count
-        self.required_travel_time = required_travel_time
+        self.r1 = r1
+        self.r2 = r2
         self.travel_times = travel_times
 
         if filename is not None:
@@ -46,27 +49,28 @@ class FilteredBaseSet(KDTreeLocationSet):
         self.latitudes = latitudes
         self.longitudes = longitudes
 
-        # indices = [i for i in range(len(latitudes)- 1)]
-        # from multiprocessing import Pool
-        # with Pool(8) as p:
-        #     bases_and_coverages = p.map(self.kmeans_filter_i, indices)
-        #
-        #
-        #
-        # from IPython import embed; embed()
-        # return None
+        indices = [i for i in range(len(latitudes)- 1)]
+        from multiprocessing import Pool
+        with Pool(8) as p:
+            bases_and_coverages = p.map(self.kmeans_filter_i, indices)
+
+
+        from IPython import embed; embed()
+        return None
 
 
 
 
-    # def kmeans_filter_i(self, i):
+    def kmeans_filter_i(self, i):
 
-        i = 0
+        # i = 0
 
         np_travel_times = np.array(self.travel_times.times)
         chosen_latitudes = []
         chosen_longitudes = []
-        demands_covered = 0
+        primary_demands_covered = 0
+        chosen_bases = []
+
 
         first_time = True
 
@@ -75,24 +79,66 @@ class FilteredBaseSet(KDTreeLocationSet):
             if not first_time: i = 0
 
             # Make a True/False table of the travel_times and then count how many covered.
-            covered = [[t < self.required_travel_time for t in row] for row in np_travel_times]
-            count_covered = [(index, covered[index].count(True)) for index in range(len(covered))]
+            primary_covered = [[t < self.r1 for t in row] for row in np_travel_times]
+            # secondary_count = [[t < self.r2 for t in row] for row in np_travel_times]
+            count_covered = [(index,
+                              primary_covered[index].count(True),
+                              # secondary_count[index].count(True)
+                              ) for index in range(len(primary_covered))
+                             ]
             d = [('index', int), ('covered', int)]
             count_covered = np.array(count_covered, d)
 
             # Sort the table by row and grab the last element (the base with the most coverage)
-            (best_base, count) = np.sort(count_covered, order='covered', kind='mergesort')[-1 - i]
+            (best_base, primary_count) = \
+                np.sort(count_covered, order='covered', kind='mergesort')[-1 - i]
+
             first_time = False
-            # print(best_base, count)
+            # print(best_base, primary_count)
             chosen_latitudes.append(self.latitudes[best_base])
             chosen_longitudes.append(self.longitudes[best_base])
-            demands_covered += count
-            demand_coverage = covered[best_base]
+            primary_demands_covered += primary_count
+            # secondary_demands_covered += secondary_count
+            demand_coverage = primary_covered[best_base]
+
+            chosen_bases.append(best_base)
+
 
             # Delete the covered columns
             delete_cols = [d for d in range(len(demand_coverage)) if demand_coverage[d]]
             np_travel_times = np.delete(np_travel_times, delete_cols, axis=1)
 
-        if demands_covered >= 99: print(demands_covered)
-        # return chosen_latitudes, chosen_longitudes, demands_covered
-        return chosen_latitudes, chosen_longitudes
+
+        # Make another copy of the travel times with only the rows of the best bases
+        # Really, we should be using the metric version but it seems to not really fit in with this part.
+        secondary_demands_covered = 0
+        new_matrix = [self.travel_times.times[i] for i in range(len(self.travel_times.times)) if i in chosen_bases]
+
+        for demand_id in range(len(new_matrix[0])):
+            # Determine if there is a primary coverage and a secondary coverage < r2
+            primary = None
+            for base_times in new_matrix:
+                if base_times[demand_id] < self.r1:
+                    primary = base_times[demand_id]
+
+            if not primary:
+                continue
+
+            secondary = False
+            for base_times in new_matrix:
+                if base_times[demand_id] < self.r2:
+                    if base_times[demand_id] != primary:
+                        secondary = True
+
+            if secondary:
+                secondary_demands_covered += 1
+
+
+        print(primary_demands_covered, secondary_demands_covered, primary_demands_covered - secondary_demands_covered)
+
+        if primary_demands_covered < secondary_demands_covered:
+            raise Exception("The secondary coverage should always be <= primary coverage.")
+
+        return chosen_latitudes, chosen_longitudes, primary_demands_covered, secondary_demands_covered
+        # return chosen_latitudes, chosen_longitudes
+
