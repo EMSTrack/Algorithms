38043f253cffb751a5004aa4ce7321e905b022fa
Hans Yuan <hansy@surveymonkey.com>
2019-02-14 13:04:35 -0800

    sync

--- ems/algorithms/selection/dispatch_least_cost.py
+++ ems/algorithms/selection/dispatch_least_cost.py
@@ -7,22 +7,24 @@ from ems.datasets.travel_times.travel_times import TravelTimes
 from ems.models.ambulances.ambulance import Ambulance
 from ems.models.cases.case import Case
 from ems.analysis.metrics.coverage.percent_coverage import PercentCoverage
+from ems.algorithms.selection.dispatch_fastest import BestTravelTime
 
 # An implementation of a "fastest travel time" ambulance_selection from a base to
 # the demand point closest to a case
-class LeastDisruption(AmbulanceSelector):
+class LeastDisruption(BestTravelTime):
 
     def __init__(self,
                  travel_times: TravelTimes = None,
                  demands = None,
                  r1 = 600,
                  ):
-        self.travel_times = travel_times
+        # self.travel_times = travel_times
         self.coverage = PercentCoverage(
             demands=demands,
             travel_times=self.travel_times,
             r1=r1
         )
+        super().__init__(travel_times=travel_times)
 
     def select_ambulance(self,
                          available_ambulances: List[Ambulance],
@@ -30,11 +32,6 @@ class LeastDisruption(AmbulanceSelector):
                          current_time: datetime):
 
         case_priority = case.priority
-
-
-        # Compute the closest demand point to the case location
-        # loc_set_2 = self.travel_times.destinations
-        # closest_loc_to_case, _, _ = loc_set_2.closest(case.incident_location)
 
         # Select an ambulance that disrupts the coverage the least regardless of travel time.
         chosen_ambulance, ambulance_travel_time = self.find_least_disruption(available_ambulances)
@@ -66,12 +63,6 @@ class LeastDisruption(AmbulanceSelector):
                 chosen_ambulance_set = ambulance_set
 
         chosen_ambulance = [ambulance for ambulance in ambulances if ambulance not in chosen_ambulance_set][0]
-
-        # from IPython import embed;
-        # embed()
-            # for ambulance in ambulance_set:
-            #     self.coverage._remove_ambulance_coverage(ambulance)
-
 
         return chosen_ambulance, None
 
